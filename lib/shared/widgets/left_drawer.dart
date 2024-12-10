d9e6b7fe5cf0d0d05a40ec21144904d3dd854e4c
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-12-10 21:02:36 +0700

    refactor: tidy up drawer code for clarity and readability

--- lib/shared/widgets/left_drawer.dart
+++ lib/shared/widgets/left_drawer.dart
@@ -16,105 +16,11 @@ class LeftDrawer extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Theme.of(context).colorScheme.primary,
-                  Theme.of(context).colorScheme.primary.withOpacity(0.7),
-                ],
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/logo.png',
-                  height: 70,
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  "Roso Jogja Mobile",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    letterSpacing: 1.1,
-                  ),
-                )
-              ],
-            ),
-          ),
-          if (authProvider.isLoggedIn)
-            // Logged-in User Section
-            ListTile(
-              leading: user?.profilePicture == null
-                  ? CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      child: Text(
-                        user!.username[
-                            0], // Use the first letter of the user's name
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    )
-                  : CircleAvatar(
-                      radius: 20,
-                      backgroundImage: NetworkImage(
-                          '${AppConfig.apiUrl}${user!.profilePicture!}'),
-                    ),
-              title: Text(
-                user.username,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                user.address,
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey,
-                ),
-              ),
-              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-            ),
-          if (!authProvider.isLoggedIn)
-            ListTile(
-              leading: Icon(
-                Icons.login,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              title: Text(
-                'Log In',
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              onTap: () => context.push('/login'),
-              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-            ),
+          _buildDrawerHeader(context),
+          if (authProvider.isLoggedIn) _buildLoggedInUserTile(context, user!),
+          if (!authProvider.isLoggedIn) _buildLoginTile(context),
           const Divider(),
-          // Motivational Text
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Text(
-              'Ayo makan-makan!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.grey[600],
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          SizedBox(height: 16),
+          _buildMotivationalText(),
           _buildDrawerItem(
             context,
             icon: Icons.home,
@@ -132,31 +38,137 @@ class LeftDrawer extends StatelessWidget {
               context,
               icon: Icons.logout,
               title: 'Logout',
-              onTap: () async {
-                final response = await authProvider.cookieRequest.logout(
-                  '${AppConfig.apiUrl}/mobile_logout/',
-                );
-                String message = response["message"];
-                if (context.mounted) {
-                  if (response['status']) {
-                    String uname = response["username"];
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text("$message. Sampai jumpa, $uname."),
-                    ));
-                    context.go('/');
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(message),
-                      ),
-                    );
-                  }
-                }
-              },
+              onTap: () => _handleLogout(context, authProvider),
             ),
         ],
       ),
     );
+  }
+
+  Widget _buildDrawerHeader(BuildContext context) {
+    return DrawerHeader(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Theme.of(context).colorScheme.primary,
+            Theme.of(context).colorScheme.primary.withOpacity(0.7),
+          ],
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/logo.png',
+            height: 70,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            "Roso Jogja Mobile",
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              letterSpacing: 1.1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoggedInUserTile(BuildContext context, dynamic user) {
+    return ListTile(
+      leading: user.profilePicture == null
+          ? CircleAvatar(
+              radius: 20,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              child: Text(
+                user.username[0],
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          : CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage(
+                '${AppConfig.apiUrl}${user.profilePicture}',
+              ),
+            ),
+      title: Text(
+        user.username,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      subtitle: Text(
+        user.address,
+        style: const TextStyle(
+          fontSize: 14,
+          color: Colors.grey,
+        ),
+      ),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+    );
+  }
+
+  Widget _buildLoginTile(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        Icons.login,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      title: const Text(
+        'Log In',
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      onTap: () => context.push('/login'),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+    );
+  }
+
+  Widget _buildMotivationalText() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Text(
+        'Ayo makan-makan!',
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          color: Colors.grey,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _handleLogout(
+      BuildContext context, AuthProvider authProvider) async {
+    final response = await authProvider.cookieRequest.logout(
+      '${AppConfig.apiUrl}/mobile_logout/',
+    );
+    final message = response["message"];
+
+    if (context.mounted) {
+      final snackBar = SnackBar(content: Text(message));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      if (response['status']) {
+        final uname = response["username"];
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("$message. Sampai jumpa, $uname."),
+        ));
+        context.go('/');
+      }
+    }
   }
 
   Widget _buildDrawerItem(
