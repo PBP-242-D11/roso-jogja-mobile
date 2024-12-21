c8672de3d344a8c21ece1ea00f69d5214956b2d4
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-12-21 19:33:34 +0700

    fix: error on null user

--- lib/shared/widgets/left_drawer.dart
+++ lib/shared/widgets/left_drawer.dart
@@ -22,7 +22,7 @@ class LeftDrawer extends StatelessWidget {
             _buildDrawerHeader(context),
 
             // User Profile Section
-            if (authProvider.isLoggedIn) _buildLoggedInUserTile(context, user!),
+            if (user != null) _buildLoggedInUserTile(context, user),
 
             // Login Prompt for Non-Logged In Users
             if (!authProvider.isLoggedIn) _buildLoginTile(context),
